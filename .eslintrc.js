0e6ccb778c5e8051a38cefc86234d040c95fb7e7
yuanbin <yuanbin.kk@bytedance.com>
2021-03-28 15:39:44 +0800

    feat: blog init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.js', '.tsx'] }],
+  },
+};
